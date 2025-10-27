2555c907dd0c587c275125f4ffb8011270c7cbb4
victor99a <vi.barreraj@duocuc.cl>
2025-10-27 19:59:33 -0300

    first commit

---
+++ src/main/java/model/Usuario.java
@@ -0,0 +1,33 @@
+package model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Table(name = "usuario")
+@Data //metodos
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)// Autoincrementacion
+    private Integer id;
+
+    @Column(nullable = false)
+    private String nombre;
+    @Column(nullable = false)
+    private String apellido;
+    @Column(nullable = false)
+    private String correo;
+    @Column(nullable = false)
+    private Integer telefono;
+    @Column(nullable = false)
+    private String contrasena;
+
+
+
+}
+
---
+++ src/test/java/duoc/example/demo/GithubApplicationTests.java
@@ -0,0 +1,13 @@
+package duoc.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
